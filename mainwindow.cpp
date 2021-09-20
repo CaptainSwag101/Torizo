7cfd205643567380a10691e0dbb786d6afcc49df
James Pelster <jpmac26@gmail.com>
2021-09-20 13:06:04 -0700

    Recreate the project with qmake instead of CMake CMake was having lots of issues when the Qt version changed, so let's see if using qmake is more reliable...

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,17 +1,11 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-#include "editors/oam_sprite_editor/oam_sprite_creator.h"
-#include "editors/room_editor/room_editor.h"
-
-MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow)
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    
-    setWindowFlag(Qt::MSWindowsFixedSizeDialogHint, true);
-    setWindowFlag(Qt::Dialog, true);
-    setFixedSize(size());
-    statusBar()->setSizeGripEnabled(false);
 }
 
 MainWindow::~MainWindow()
@@ -19,86 +13,3 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void MainWindow::on_roomEditorButton_clicked()
-{
-    // First, check to make sure RoomEditor isn't already open
-    for (std::shared_ptr<QWidget> &editorPtr : activeEditors)
-    {
-        if (editorPtr.get()->windowTitle() == tr("Room Editor"))
-        {
-            editorPtr.get()->show();
-            return;
-        }
-    }
-    
-    std::shared_ptr<RoomEditor> editorPtr = std::make_shared<RoomEditor>(this);
-    activeEditors.append(editorPtr);
-    editorPtr.get()->show();
-}
-
-void MainWindow::on_oamSpriteCreatorButton_clicked()
-{
-    // First, check to make sure OAMSpriteCreator isn't already open
-    for (std::shared_ptr<QWidget> &editorPtr : activeEditors)
-    {
-        if (editorPtr.get()->windowTitle() == tr("OAM Sprite Creator"))
-        {
-            editorPtr.get()->show();
-            return;
-        }
-    }
-    
-    std::shared_ptr<OAMSpriteCreator> editorPtr = std::make_shared<OAMSpriteCreator>(this);
-    activeEditors.append(editorPtr);
-    editorPtr.get()->show();
-}
-
-void MainWindow::on_action_Open_ROM_triggered()
-{
-    QString romPath = QFileDialog::getOpenFileName(this, tr("Open ROM"), QString(), tr("SNES ROM Files (*.smc *.sfc);;All Files (*.*)"));
-    if (!romPath.isEmpty())
-    {
-        OpenROM(romPath);
-    }
-}
-
-void MainWindow::OpenROM(QString romPath)
-{
-    if (!QFile::exists(romPath))
-    {
-        QMessageBox errorMsg(QMessageBox::Warning, tr("Error"), tr("Specified ROM file does not exist"), QMessageBox::Ok);
-        errorMsg.exec();
-        return;
-    }
-    
-    QFile romFile = QFile(romPath);
-    romFile.open(QFile::ReadOnly);
-    QByteArray raw = romFile.readAll();
-    
-    LoadedROMPath = romPath;
-    
-    // Check if rom is headered
-    if (raw.size() % 0x10000 > 0)
-    {
-        // Header and data
-        ROMHeader = raw.left(512);
-        ROMData = raw.right(raw.size() - 512);
-    }
-    else
-    {
-        // Data only
-        ROMHeader = QByteArray();
-        ROMData = raw;
-    }
-    
-    
-    
-    // Import all relevant data from ROM
-    LoadTilesets();
-    LoadRooms();
-    
-    
-    // Enable editor buttons once a ROM is loaded
-    ui->roomEditorButton->setEnabled(true);
-    ui->oamSpriteCreatorButton->setEnabled(true);
-}
--- mainwindow.h
+++ mainwindow.h
@@ -1,36 +1,21 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
-#include "rom_utils/tileset.h"
-
 #include <QMainWindow>
-#include <QFile>
-#include <QFileDialog>
-#include <QList>
-#include <QMessageBox>
-#include <QWidget>
 
-namespace Ui {
-class MainWindow;
-}
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
-    
+
 public:
-    explicit MainWindow(QWidget *parent = nullptr);
+    MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
-    void OpenROM(QString romPath);
-    
-private slots:
-    void on_roomEditorButton_clicked();
-    void on_oamSpriteCreatorButton_clicked();
-    void on_action_Open_ROM_triggered();
-    
+
 private:
     Ui::MainWindow *ui;
-    QList<std::shared_ptr<QWidget>> activeEditors;
 };
-
 #endif // MAINWINDOW_H
